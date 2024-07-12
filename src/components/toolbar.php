eac16a95eacf99d1665d5fd9a42c5ce6bf175afc
iballidev <iballi.dev@gmail.com>
2024-07-12 21:20:41 +0100

    update

--- src/components/toolbar.php
+++ src/components/toolbar.php
@@ -18,6 +18,7 @@
                     <li>
                         <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-sm text-center text-on-theme-clr-primary inline-flex items-center" type="button">
                             Select Country
+                            <img src="https://lr.instantbillspay.com/assets/common/img/flag/lr.gif" alt="">
                             <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
                             </svg>
