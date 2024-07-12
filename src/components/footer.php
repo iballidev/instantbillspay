653cc4ad10333cddff446718bccdeef61af298ed
iballidev <iballi.dev@gmail.com>
2024-07-12 22:53:35 +0100

    update

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,6 +1,6 @@
 <div class="h-40"></div>
 
-<footer class="px-4 pt-8 pb-4 fixed z-0 bottom-0 left-0 right-0 bg-[#192223] text-on-theme-clr-primary text-center">
+<footer class="px-4 pt-8 pb-4 bg-[#192223] text-on-theme-clr-primary text-center">
     <p class="text-on-theme-clr-primary font-semibold">
         Copyright © 2024 Instant Bills Pay. Powered By: United Bank for Africa. Licensed by the Central Bank of Nigeria.
     </p>
