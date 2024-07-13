bbe61ae118d0d25b6f264d4f22cec4cd0004e0bd
iballi2017 <iballi201@gmail.com>
2024-07-13 23:20:34 +0100

    update

--- src/views/transactions.php
+++ src/views/transactions.php
@@ -5,7 +5,7 @@
 
 
     <h1 class="text-xl font-bold mb-2">Transaction Updates</h1>
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div class="relative overflow-x-auto">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
                 <tr>
