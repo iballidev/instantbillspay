30f854eb0fcfba345b23d77bc2632ea9be4de0ff
iballidev <iballi.dev@gmail.com>
2024-07-15 12:27:22 +0100

    update

--- src/mobile-and-toll-e-top-up-details.php
+++ src/mobile-and-toll-e-top-up-details.php
@@ -58,11 +58,11 @@
                                 </div>
 
 
+
                                 <button type="submit" class="text-white bg-theme-clr-primary hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-8 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                                     Buy Now
                                 </button>
                             </form>
-
                         </div>
                     </div>
                 </div>
